3fb0fed099478350f5093daf827282aea71fa084
kusogray <kusogray1@gmail.com>
2016-01-24 18:54:50 +0800

    Init
    Init

--- Curiosity/ExperimentPlayGround.py
+++ Curiosity/ExperimentPlayGround.py
@@ -9,10 +9,11 @@ from Telstra.DataAnalyzer.ModelFactory import ModelFactory as fab
 if __name__ == '__main__':
     
     path = "/Users/whmou/Kaggle/Telstra/train_merge7.csv"
-    
+    testPath = ""
     # 1. read data
     dr = dr()
     dr.readInCSV( path, "train")
+    dr.readInCSV(testPath , "test")
     
     # 2. run models
     #print dr._trainDataFrame.as_matrix
--- DataCollector/DataProcessor.py
+++ DataCollector/DataProcessor.py
@@ -12,67 +12,42 @@ class DataProcessor(object):
     classdocs
     '''
     
-    _eventTypePath = "/Users/whmou/Kaggle/Telstra/log_feature.csv"
-    _pathMain = "/Users/whmou/Kaggle/Telstra/train_merge5.csv"
-    _outputPathName = "/Users/whmou/Kaggle/Telstra/train_merge6.csv"
+    _eventTypePath = "/Users/whmou/Kaggle/Telstra/resource_type.csv"
+    _pathMain = "/Users/whmou/Kaggle/Telstra/test2.csv"
+    _outputPathName = "/Users/whmou/Kaggle/Telstra/test3.csv"
 
     def __init__(self):
         '''
         Constructor
         '''
-    def mergeData(self):
-        dr = DataReader()
-        dr.readInCSV(self._eventTypePath, "train")
-        
-        tmpColumnPrefix = "resource_type_"
-        df = pd.read_csv(self._eventTypePath, header=0, sep=',')
-        
-        for i in range (1,54):
-            tmpColName = tmpColumnPrefix + str(i)
-            dr._trainDataFrame[tmpColName] = 0
-    
-    
-        for i1 in range(0, len(dr._trainDataFrame[dr._trainDataFrame.columns[0]] )):
-            for i2 in range(0, len(df[df.columns[0]] )):
-                tmpMainId = dr._trainDataFrame[dr._trainDataFrame.columns[0]][i1]
-                tmpId = df[df.columns[0]][i2]
-                if  tmpMainId == tmpId:
-                    tmpVal = df[df.columns[1]][i2]
-                    tmpColName = tmpColumnPrefix + str(tmpVal)
-                    dr._trainDataFrame[tmpColName][i1] = 1
-                if tmpMainId > tmpId:
-                    break
-    
-        print dr._trainDataFrame  
-    
     
     def sumExist(self):
         dr = DataReader()
-        dr.readInCSV(self._pathMain, "train")
-        tmpColumnPrefix = "log_feature_"
+        dr.readInCSV(self._pathMain, "test")
+        tmpColumnPrefix = "resource_type_"
         df = pd.read_csv(self._eventTypePath, header=0, sep=',')
-        
-        for i in range (2,3):
+        processDf = dr._testDataFrame
+        for i in range (1,2):
             tmpColName = tmpColumnPrefix + str(i)
-            dr._trainDataFrame[tmpColName] = 0
+            processDf[tmpColName] = 0
         
         tmpLastI2 = 0
-        for i1 in range(0, len(dr._trainDataFrame[dr._trainDataFrame.columns[0]] )):
+        for i1 in range(0, len(processDf[processDf.columns[0]] )):
 
             tmpFlag = False
             for i2 in range(tmpLastI2, len(df[df.columns[0]] )):
-                tmpMainId = dr._trainDataFrame[dr._trainDataFrame.columns[0]][i1]
+                tmpMainId = processDf[processDf.columns[0]][i1]
                 tmpId = df[df.columns[0]][i2]
-                tmpVal= df[df.columns[2]][i2]
+                tmpVal= df[df.columns[1]][i2]
                 if  tmpMainId == tmpId:
                     tmpFlag = True
-                    dr._trainDataFrame[dr._trainDataFrame.columns[6]][i1] +=tmpVal 
+                    processDf[processDf.columns[2]][i1] +=1 
                 if tmpFlag == True and tmpMainId != tmpId:
-                    print "der"
                     tmpLastI2 = i2
                     break
                 #print i1, i2
-        outDf = pd.concat([dr._ansDataFrame, dr._trainDataFrame], axis=1)
+        #outDf = pd.concat([dr._ansDataFrame, processDf], axis=1)
+        outDf = processDf
         outDf.to_csv(self._outputPathName, sep=',', encoding='utf-8')  
         #print dr._ansDataFrame
         
